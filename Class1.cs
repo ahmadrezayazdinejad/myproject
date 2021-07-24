f11b0ddd4e56c53453e41d3e80ffaf1d679aabad
Ahmadreza <Ahmadrezayazdinejad1379@gmail.com>
2021-07-24 10:39:53 -0700

    project 7

--- Class1.cs
+++ Class1.cs
@@ -6,8 +6,4 @@
     another brother :mahdi
     sisters name:i dont have any sister
     sisters name:i dont have any sister
-<<<<<<< HEAD
     the ahmadreza
-=======
-    tha ahmadreza
->>>>>>> developer
